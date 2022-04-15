704e162010cc0a0d3d317b0f331104a7b0b267f7
Isaac Abarca <abarcai532@gmail.com>
2022-04-15 00:21:28 -0500

    Agregar archivos de proyecto.

---
+++ CapaConexion/servicio.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace SistemaVeterinaria.CapaConexion
+{
+    public class servicio
+    {
+        private SqlConnection conexion;
+
+        public servicio()
+        {
+            conexion = new SqlConnection(@"user=DESKTOP-SHJTAP0\UCR; 
+                                           pasword= validpassword;
+                                           server = DESKTOP-SHJTAP0;
+                                           Trusted_Connection =yes;
+                                           database = SistemaVeterinaria;
+                                           connection timeout 30");
+        }
+    }
+}
